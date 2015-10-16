a4ebd62ebc3c2a64fac127adc6192f1cd1685c66
Hardys- <hardys1206@gmail.com>
2015-10-16 03:23:10 -0400

    Initialization

---
+++ js/csChart.js
@@ -0,0 +1,3 @@
+
+function test(){http://code.tutsplus.com/tutorials/build-your-first-javascript-library--net-26796}
+
---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+function recmmendation(){}
+
+function drawChart()
+
+function updateName(){}
+
+function undo(){}
+
+function checkOut(){}
+
+function add(){}
+
+function save(){}// save to local & serverside
+
+function load(){}// find user credential(in XML) and load data(from a json file named as userName.json)
+
+function logIn(){}// encryped user info back-end php code
+
+
