cde9c9ee2b20029581e39f97176ef1ec0d9c96ed
Hardys- <hardys1206@gmail.com>
2015-10-18 22:18:56 -0400

    Framework of Shareculator: update Initial3

--- js/cal.js
+++ js/cal.js
@@ -1,17 +1,24 @@
-class Sharer{
-	this.cost = 0;	
-	this.paid = 0;
-	this.trans =[];
-	getCost(){
-		return cost;
-	}
+var sharer = function(){
+    this.cost = 0;
+    this.paid = 0;
+    this.paidList = [];
+    this.costList = [];
+    
+    this.pay=function(amount){
+    	this.paid += amount;
+	this.paidList.push(amount);
+    };
+    
+    this.cost=function(amount) {
+    	this.cost += amount;
+	this.costList.push(amount);
+    };
 
-	getPaid(){
-		return paid;
-	}
-
-	add(amount){
-		trans.push(amount);
-	}
+    this.getPaid=function(){
+    	return this.paid;
+    };
+    
+    this.getCost=function(){
+    	return this.cost;
+    };
 }
-
--- js/index.js
+++ js/index.js
@@ -9,7 +9,32 @@ function undo(){}
 
 function checkOut(){}
 
-function add(){}
+function add(){
+	/*add options*/
+
+	var optionString = "<option value=\""+$("#addNewSharerText").val()+"\" selected>"+$("#addNewSharerText").val()+"</option>";
+	
+	if($("#sharer :selected").text() == "Add a sharer"){
+		$("#sharer").html(optionString);
+		$("#sharer-list").append("Shared with &nbsp;");
+	}else{
+		$("#sharer").append(optionString);
+	}	
+
+	/*add checkboxs*/
+	var checkboxString = "<input type=\"checkbox\" id=\""+$("#sharer :selected").val()+"\" value=\""+$("#sharer :selected").val()+"\" checked>"+$("#sharer :selected").val()+"&nbsp;";
+	$("#sharer-list").append(checkboxString);
+
+	/*add new instance*/
+
+
+	if( $("#sharer :selected").text() == $("#addNewSharerText").val()){
+	       	notification($("#addNewSharerText").val()+" has been added",1);
+	}	
+	$("#add-sharer-panel").fadeOut(300);
+	$("#addNewSharerText").val("");	
+	
+}
 
 function save(){}// save to local & serverside
 
@@ -17,28 +42,91 @@ function load(){}// find user credential(in XML) and load data(from a json file 
 
 function logIn(){}// encryped user info back-end php code
 
+function notification(str,flag){
+      var htmlString ="";
+      if(flag){
+          htmlString = "<p class = \"msg-content\">&nbsp;&nbsp;<span class=\"attention1\">&#10003</span>&nbsp;&nbsp;"+str+"</p>";
+      }else{
+          htmlString = "<p class = \"msg-content\">&nbsp;&nbsp;<span class=\"attention2\">!</span>&nbsp;&nbsp;"+str+"</p>";
+      }
+      $("#msg").html(htmlString);
+      if(detectmob()){
+        $("#msg").css({"position":"absolute","width":"100%","left":"0px","top":"0px","height":"10%"});
+        $("#msg").fadeIn( 1000 ).fadeOut( 2000 );
+        $("#msg p").css({"font-size":"4em","margin-top":"5%"});
+        $("#msg span").css({"height":"4em","width":"4em","line-height":"5em"});
+      }else{
+        $("#msg").css({"position":"absolute","width":"20%","left":"40%","top":"40px"});
+        $("#msg").fadeIn( 1000 ).fadeOut( 2000 );
+      };
+}
 
+function detectmob() {
+   if( navigator.userAgent.match(/Android/i)
+   || navigator.userAgent.match(/webOS/i)
+   || navigator.userAgent.match(/iPhone/i)
+   || navigator.userAgent.match(/iPod/i)
+   || navigator.userAgent.match(/BlackBerry/i)
+   || navigator.userAgent.match(/Windows Phone/i)
+   ){
+      return true;
+    }
+   else {
+      return false;
+    }
+}
 
 $( document ).ready(function() {
 	$(".submit").hover(function(){
 			$(this).stop().animate({
 				opacity:"0.8"
-				}, 800);
+				},300); //default 800
 			},function(){
 				$(this).stop().animate({
 				opacity:"1"
-			}, 800);
+			}, 300);//default 800
 	})
 
-	$("#logInButton").click(function(){
-		$("#front-panel").fadeOut(800,function(){$("#login-panel").fadeIn(800);})
+	$("#logInButton").click(function(){//default 800
+		$("#front-panel").fadeOut(300,function(){$("#login-panel").fadeIn(300);})
 		$("p.title").html("Please enter your user name and password:");	
 	});
 
-	$("#createButton").click(function(){
+	$("#createButton").click(function(){//default 800
+		$("#front-panel").fadeOut(300,function(){$("#main-panel").fadeIn(300);$("p.title").css({"display":"none"});})		
+	});
+
+	$("#addSharerButton").click(function(){// open add sharer panel
+		/*add panel*/
+    		$("#add-sharer-panel").css({"position":"absolute","left":"30%","top":"50%"});
+		$("#add-sharer-panel").fadeIn(300);//default 800
+		$("#addNewSharerText").focus();	
+	});
+
+	$("#addNewSharerCancelButton").click(function(){ //exit current panel
+		$("#add-sharer-panel").fadeOut(300);
+		$("#addNewSharerText").val("");
+	});
+
+	$("#addNewSharerButton").click(function(){ //add new sharer
+		if($("#addNewSharerText").val() != ""){
+			add();
+		}else{
+			notification("Please type in a name",0);
+		}
 		
-		$("#front-panel").fadeOut(800,function(){$("#main-panel").fadeIn(800);$("p.title").css({"display":"none"});})		
 	});
 	
+	$("#addNewSharerText").keydown(function(event) {
+    		if (event.keyCode == 13) {
+        		$("#addNewSharerButton").click();
+        	}
+	});
+
+	var hh = new sharer();
+	hh.pay(50);
+        hh.pay(80);
+	alert(hh.getPaid());
+	alert(hh.paidList[0]);
 });
 
