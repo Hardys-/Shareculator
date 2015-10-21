7def4e6fde1affb9e187dbedea8f00fb3db1486f
Hao <hardys1206@gmail.com>
2015-10-21 18:39:45 -0400

    Checkout function implementation

--- js/cal.js
+++ js/cal.js
@@ -32,7 +32,7 @@ var sharer = function(name){
 var jsonData = {
 	"sharerName":[],          //e.g. [sharerName1,sharerName2,sharerName3,sharerName4]
 	"sharerCosts":[],         //e.g. [[50,60,70,18,0,0],[0,0,10,20,30,40],[12,13,14,15],[0,0,0,0]] same #items as sharerName
-	"payerList":[],	          //e.g. [sharerName2,sharerName3,sharerName4,sharerName1,sharerName2,sharerName6] 
+	"payerList":[],	          //e.g. each record:{"payer":"sharerName2","consumer":[sharerName2,sharerName3,sharerName4],"amount":"$***","memo":"string memo"} 
 }  
 
 var sharerData = [];
