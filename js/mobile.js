9ffd4afea14f66e9ed4204228986bd31e4cec9f9
HAO <hardys1206@gmail.com>
2015-11-25 00:48:19 -0500

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -1,1 +1,15 @@
 
+function detectmob() {//detect what mobile type the user is using.
+   if( navigator.userAgent.match(/Android/i)
+   || navigator.userAgent.match(/webOS/i)
+   || navigator.userAgent.match(/iPhone/i)
+   || navigator.userAgent.match(/iPod/i)
+   || navigator.userAgent.match(/BlackBerry/i)
+   || navigator.userAgent.match(/Windows Phone/i)
+   ){
+      return true;
+    }
+   else {
+      return false;
+    }
+}
