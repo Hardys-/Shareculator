ea4db7b7a0981c48d34ffcb8a2fe7e5da193d51e
HAO <hardys1206@gmail.com>
2015-11-27 00:04:26 -0500

    Update mobile.js

--- js/mobile.js
+++ js/mobile.js
@@ -14,6 +14,13 @@ function detectmob() {//detect what mobile type the user is using.
     }
 }
 
+function getParameterByName(name) {//url string query
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 
 $( document ).ready(function() {
    if(detectmob()){
