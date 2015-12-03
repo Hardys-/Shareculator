0dd18508a708c8faaeb5b243c3c8de44180cc56a
HAO <hardys1206@gmail.com>
2015-12-03 00:29:44 -0500

    Update tutorial.js

--- js/tutorial.js
+++ js/tutorial.js
@@ -1,1 +1,34 @@
 
+
+function detectmob() {//detect what mobile type the user is using.
+   if( navigator.userAgent.match(/Android/i)
+   || navigator.userAgent.match(/webOS/i)
+   || navigator.userAgent.match(/iPhone/i)
+   || navigator.userAgent.match(/iPod/i)
+   || navigator.userAgent.match(/BlackBerry/i)
+   || navigator.userAgent.match(/Windows Phone/i)
+   ){
+      return true;
+    }
+   else {
+      return false;
+    }
+}
+
+function getParameterByName(name) {//url string query
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+
+$( document ).ready(function() {
+   if(detectmob() && language == getParameterByName("en")){
+    /* Change behaviors here*/
+    // do things here
+      
+   }else if(detectmob() && language == getParameterByName("cn")){
+      
+   }
+})
