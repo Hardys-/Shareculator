6698106c10b3a8e1760ae60bd03edc7d8347da7a
HAO <hardys1206@gmail.com>
2016-05-12 10:52:59 -0400

    changed notification animation

--- js/index_cn.js
+++ js/index_cn.js
@@ -228,12 +228,12 @@ function notification(str,flag){//(notification string, 0: ! mark / 1: correct m
       }
       $("#msg").html(htmlString);
       if(detectmob()){
-        $("#msg").fadeIn( 1000 ).fadeOut( 2000 );
+        $("#msg").stop().fadeIn( 1000 ).fadeOut( 2000 );
         $("#msg p").css({"font-size":"4em","margin-top":"1%"});
         $("#msg span").css({"height":"50px","width":"50px","line-height":"1em","font-size":"1em"});   	
       }else{
         $("#msg").css({"position":"fixed","width":"20%","left":"40%","top":"40px"});
-        $("#msg").fadeIn( 1000 ).fadeOut( 2000 );
+        $("#msg").stop().fadeIn( 1000 ).fadeOut( 2000 );
       };
 }
 
